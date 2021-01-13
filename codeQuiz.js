3c6be3a0f16e3483ce3107628a31dec46db0928a
Justin Rovnak <jmrovnak@gmail.com>
2021-01-13 13:00:22 -0600

    created object/array var to hold questions

---
+++ codeQuiz.js
@@ -0,0 +1,18 @@
+var quizQuestions = [
+    {
+        "title": "What is 1 + 1?",
+        "options": ["1", "2", "3", "4"],
+        "correct": "2"
+        }, 
+    {
+        "title": "What is 2 + 2?",
+        "options": ["2", "4", "6", "8"],
+        "correct": "4"
+        },
+    {
+        "title": "What is 3 + 3?",
+        "options": ["2", "4", "6", "8"],
+        "correct": "6"
+    }
+
+]
